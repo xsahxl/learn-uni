c66c61899efe5ca9f22896aea9c22affdb3ed5c7
xsahxl <xsahxl@126.com>
2024-09-06 22:03:11 +0800

    feat: image
    Signed-off-by: xsahxl <xsahxl@126.com>

--- pages/demo1/demo1.vue
+++ pages/demo1/demo1.vue
@@ -1,20 +1,28 @@
 <template>
-	<view>
-		<swiper indicator-dots indicator-color="#007aff" indicator-active-color="#4cd964" circular autoplay>
-			<swiper-item>
-				<view class="swiper-item">111</view>
-			</swiper-item>
-			<swiper-item>
-				<view class="swiper-item">2222</view>
-			</swiper-item>
-			<swiper-item>
-				<view class="swiper-item">3333</view>
-			</swiper-item>
-			<swiper-item>
-				<view class="swiper-item">4444</view>
-			</swiper-item>
-		</swiper>
-	</view>
+	
+		<view>
+			<swiper indicator-dots indicator-color="#007aff" indicator-active-color="#4cd964" circular autoplay vertical>
+				<swiper-item>
+					<image src="../../static/pic1.png" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../static/pic2.png" mode="aspectFit"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../static/pic4.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../static/pic3.webp" mode="aspectFit"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<image class="picture" src="../../static/logo.png" mode=""></image>
+		<image class="picture" src="../../static/pic1.png" mode="aspectFill"></image>
+		<image class="picture" src="../../static/pic2.png" mode=""></image>
+		<image class="picture" src="../../static/pic4.jpg" mode=""></image>
+		<image src="../../static/pic3.webp" mode="aspectFill"></image>
+		<image src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-app.png" mode="aspectFit"></image>
+	
 </template>
 
 <script setup>
@@ -30,10 +38,18 @@
 			   width: 100%;
 			   height: 100%;
 			   background-color: pink;
+			   image{
+				   width: 100%;
+				   height: 100%;
+			   }
 		   }
 		   swiper-item:nth-child(2n){ 
 			   background-color: orange;
 		   }
 	   }    
+	   .picture{
+		   width: 150px;
+		   height: 150px;
+	   }
 </style>
  
