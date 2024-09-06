93a5f6ea38fd5e62ad51d84959c4ec5b70ed405b
xsahxl <xsahxl@126.com>
2024-09-06 21:31:03 +0800

    feat: swiper
    Signed-off-by: xsahxl <xsahxl@126.com>

--- pages/demo1/demo1.vue
+++ pages/demo1/demo1.vue
@@ -1,6 +1,19 @@
 <template>
 	<view>
-		这是demo1
+		<swiper indicator-dots indicator-color="#007aff" indicator-active-color="#4cd964" circular autoplay>
+			<swiper-item>
+				<view class="swiper-item">111</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item">2222</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item">3333</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item">4444</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,7 +21,19 @@
 	
 </script>
 
-<style>
-	       
+<style lang="scss">
+	   swiper{
+		   width: 100vw;
+		   height: 200px;
+		   border: 1px solid red;
+		   swiper-item{
+			   width: 100%;
+			   height: 100%;
+			   background-color: pink;
+		   }
+		   swiper-item:nth-child(2n){ 
+			   background-color: orange;
+		   }
+	   }    
 </style>
  
