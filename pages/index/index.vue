33f315029b335c9becd54838182487fbd956f977
xsahxl <xsahxl@126.com>
2024-09-06 19:31:59 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="layout">
+		<view class="box1">
+			box1
+		</view>
+		<view class="box2">
+			box2
+		</view>
+		
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss">
+	.layout{
+		border: 1px solid red;
+		.box1{
+			border: 1px solid yellow;
+		}
+		.box2{
+			border: 1px solid blue;
+		}
+	}
+	
+</style>
