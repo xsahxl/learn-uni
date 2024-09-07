bd9456183f125c9c5da326e99a3fbf60f2e88c40
xsahxl <xsahxl@126.com>
2024-09-07 09:03:25 +0800

    feat: vue 模版语法
    Signed-off-by: xsahxl <xsahxl@126.com>

---
+++ pages/demo3/demo3.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		{{a}}
+	</view>
+	<view class="">
+		{{Math.random()}}
+	</view>
+	<view class="">
+		{{2+3}}
+	</view>
+	<view class="">
+		{{fn()}}
+	</view>
+</template>
+
+<script setup>
+	const a = 6
+	function fn(){
+		return 'vue3 学习'
+	}
+</script>
+
+<style>
+
+</style>
