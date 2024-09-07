ef4d79f2b19048d22fcea3dcf0be82a4f259d368
xsahxl <xsahxl@126.com>
2024-09-07 10:08:24 +0800

    feat: vue v-on
    Signed-off-by: xsahxl <xsahxl@126.com>

---
+++ pages/demo6/demo6.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="box" @click="onClick">
+		{{num}}
+	</view>
+	<view>
+		<switch @change="onChange" />
+		<button type="primary" :loading="isLoading">button</button>
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+	let num = ref(0);
+	let isLoading = ref(false)
+	function onClick(){
+		num.value++;
+	}
+	function onChange(e){
+		isLoading.value = e.detail.value	
+	}
+	
+</script>
+
+<style>
+	.box{
+		width: 200px;
+		height: 200px;
+		background-color: red;
+	}
+
+</style>
