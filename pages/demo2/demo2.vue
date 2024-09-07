7893894f36b0f508c1a3424318e6500fad824582
xsahxl <xsahxl@126.com>
2024-09-07 08:47:52 +0800

    feat: button and input
    Signed-off-by: xsahxl <xsahxl@126.com>

--- pages/demo2/demo2.vue
+++ pages/demo2/demo2.vue
@@ -1,6 +1,12 @@
 <template>
 	<view>
-		这是demo2
+		<button type="default" loading>按钮</button>
+		<button type="primary">按钮</button>
+		<button type="warn">按钮</button>
+		<button size="mini" type="warn">按钮</button>
+		<view>------</view>
+		<input type="tel" placeholder="请输入电话号码" placeholder-style="color:red" />
+		<input type="text" placeholder="请输入搜索内容" placeholder-style="color:red" confirm-type="search" />
 	</view>
 </template>
 
