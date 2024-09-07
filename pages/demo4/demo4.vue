c4e77553b5c1eed03b1df955d6e159531c6133ca
xsahxl <xsahxl@126.com>
2024-09-07 09:23:18 +0800

    feat: vue ref
    Signed-off-by: xsahxl <xsahxl@126.com>

---
+++ pages/demo4/demo4.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		{{num}}
+	</view>
+	<view>{{num2}}</view>
+	<view>{{str}}</view>
+	<view>{{arr}}</view>
+	<view>{{obj}}</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+	let num = 1;
+	// setInterval(()=>{
+	// 	num++;
+	// 	console.log(num)
+	// }, 1000)
+	
+	let num2 = ref(10);
+	setInterval(()=>{
+		num2.value++;
+		console.log(num2.value);
+	}, 1000)
+	let str = ref('测试');
+	let arr = ref([1,2,3,4]);
+	let obj = ref({name: 'xiaoming', age: 20});
+	obj.value.name = 'xiaohong'
+	
+</script>
+
+<style>
+
+</style>
