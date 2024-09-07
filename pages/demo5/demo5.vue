c36159f00dc85d400c92b5ac4d26df87ffde7a42
xsahxl <xsahxl@126.com>
2024-09-07 09:39:16 +0800

    feat: vue v-bind
    Signed-off-by: xsahxl <xsahxl@126.com>

---
+++ pages/demo5/demo5.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<image :src="picture" mode=""></image>
+		<button type="primary" :loading="false">按钮</button>
+		
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+	const list = ref([
+		'../../static/pic1.png',
+		'../../static/pic2.png',
+		'../../static/pic4.jpg',
+		'../../static/pic3.webp'
+	]);
+	const picture = ref(list.value[0]);
+	
+	let i = 0;
+	setInterval(()=>{
+		i++;
+		if(i===4) i=0;
+		picture.value = list.value[i];
+	}, 1000)
+</script>
+
+<style>
+
+</style>
