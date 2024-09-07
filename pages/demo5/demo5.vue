3e0e7c377f53c04cb5b0fa8aec9c4c76112230a6
xsahxl <xsahxl@126.com>
2024-09-07 09:52:31 +0800

    feat: vue style and class
    Signed-off-by: xsahxl <xsahxl@126.com>

--- pages/demo5/demo5.vue
+++ pages/demo5/demo5.vue
@@ -2,6 +2,15 @@
 	<view>
 		<image :src="picture" mode=""></image>
 		<button type="primary" :loading="false">按钮</button>
+		<view class="box" :class="{active: isActive}">
+			class测试
+		</view>
+		<view class="box" :class="0 ? 'active': ''">
+			class测试
+		</view>
+		<view class="box" :style="{width: '300px'}">
+			style测试
+		</view>
 		
 	</view>
 </template>
@@ -15,15 +24,25 @@ import { ref } from 'vue';
 		'../../static/pic3.webp'
 	]);
 	const picture = ref(list.value[0]);
+	const isActive = ref(false);
 	
 	let i = 0;
 	setInterval(()=>{
 		i++;
 		if(i===4) i=0;
 		picture.value = list.value[i];
+		isActive.value = !isActive.value;
 	}, 1000)
 </script>
 
 <style>
+	.box{
+		width: 200px;
+		height: 200px;
+		background-color: red;
+	}
+	.active{
+		background-color: yellow;
+	}
 
 </style>
