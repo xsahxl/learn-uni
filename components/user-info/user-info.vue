96fa45efb7f75dc27e4608d4a41670644a71deac
xsahxl <xsahxl@126.com>
2024-09-09 07:56:36 +0800

    feat: vue components
    Signed-off-by: xsahxl <xsahxl@126.com>

---
+++ components/user-info/user-info.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		{{username}}
+	</view>
+</template>
+
+<script setup>
+// import { computed, ref } from 'vue';
+// const props = defineProps(['username'])
+// const name = computed(()=>props.username+'7')
+defineProps({
+	username:{
+		type: String,
+		// default: '匿名' 
+		default(){
+			return '匿名';
+		}
+	}
+});
+</script>
+
+<style>
+
+</style>
---
+++ pages/demo11/demo11.vue
@@ -0,0 +1,15 @@
+<template>
+	<view>
+		<user-info username='小明'></user-info>
+		<user-info></user-info>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
