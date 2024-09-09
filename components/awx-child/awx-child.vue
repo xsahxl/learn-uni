b09273fd8079e729a2a9ee7cdd6c2428a89b9d1c
xsahxl <xsahxl@126.com>
2024-09-09 09:54:09 +0800

    feat: vue emit
    Signed-off-by: xsahxl <xsahxl@126.com>

---
+++ components/awx-child/awx-child.vue
@@ -0,0 +1,18 @@
+<template>
+	<view>
+		子组件
+		<!-- <button @click="$emit('add', 123)">按钮</button> -->
+		<button @click="onAdd">button</button>
+	</view>
+</template>
+
+<script setup>
+	const emit = defineEmits(['add'])
+	function onAdd(){
+		emit('add', 123)
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/demo13/demo13.vue
@@ -0,0 +1,17 @@
+<template>
+	<view>
+		<awx-child @add="onAdd"></awx-child>
+	</view>
+</template>
+
+<script setup>
+	function onAdd(v){
+		console.log(v)
+	}
+	
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
