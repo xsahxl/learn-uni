1b0179e00547b4ca784c0b8554e74d6d5efe7066
xsahxl <xsahxl@126.com>
2024-09-09 14:42:56 +0800

    feat: vue onHide
    Signed-off-by: xsahxl <xsahxl@126.com>

--- pages/demo15/demo15.vue
+++ pages/demo15/demo15.vue
@@ -7,14 +7,24 @@
 			{{person.name}}
 		</view>
 		<navigator url="/pages/demo14/demo14">跳转demo14</navigator>
+		<view class="">
+			{{count}}
+		</view>
 	</view>
 </template>
 
 <script setup>
-import { onLoad, onReady, onShow } from '@dcloudio/uni-app';
+import { onLoad, onReady, onShow, onHide } from '@dcloudio/uni-app';
 import { ref } from 'vue';
 const person = ref(null);
 const scroll = ref(null)
+
+const count = ref(0);
+
+let timer = setInterval(()=>{
+	count.value++;
+}, 100);
+
 onLoad((e)=>{
 	console.log('onLoad', e);
 	person.value = e;
@@ -22,7 +32,16 @@ onLoad((e)=>{
 })
 
 onShow(()=>{
-	console.log('onShow')
+	console.log('onShow', count.value)
+	timer = setInterval(()=>{
+		count.value++;
+	}, 1000)
+	
+})
+
+onHide(()=>{
+	console.log('onHide', count.value)
+	clearInterval(timer)
 })
 
 onReady(()=>{
