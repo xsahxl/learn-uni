6b3e88896c6543745559498149e97497e33680f4
xsahxl <xsahxl@126.com>
2024-09-10 09:36:21 +0800

    feat: uni.showToast
    Signed-off-by: xsahxl <xsahxl@126.com>

---
+++ pages/demo16/demo16.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>demo16</view>
+	<button @click="show" >show</button>
+	<button @click="hide" >hide</button>
+</template>
+
+<script setup>
+function show(){
+	uni.showToast({
+		title:'操作成功',
+		duration:2000
+	})
+}
+
+function hide(){
+	uni.hideToast()
+}
+	
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
